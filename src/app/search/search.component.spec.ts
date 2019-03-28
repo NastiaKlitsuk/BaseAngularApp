feecbb2a41d1395b131c8178ac02c6170ae3bc6a
Nastia Klitsuk <nastiak@codevalue.net>
2019-03-28 16:12:39 +0200

    a new test added

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -2,10 +2,11 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { SearchComponent } from "./search.component";
 import { MatIconModule } from "@angular/material/icon";
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { RouterTestingModule } from '@angular/router/testing';
-import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { RouterTestingModule } from "@angular/router/testing";
+import { MatInputModule } from "@angular/material/input";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { By } from "@angular/platform-browser";
 
 describe("SearchComponent", () => {
   let component: SearchComponent;
@@ -13,7 +14,13 @@ describe("SearchComponent", () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, NoopAnimationsModule, MatIconModule, MatFormFieldModule, MatInputModule],
+      imports: [
+        RouterTestingModule,
+        NoopAnimationsModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule
+      ],
       declarations: [SearchComponent]
     }).compileComponents();
   }));
@@ -26,5 +33,24 @@ describe("SearchComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should erase search event when clicked", () => {
+    let searchQuery: string;
+    let expectedSearchQuery = "dress";
+
+    let searchInputElement = fixture.debugElement.nativeElement.querySelector(
+      "input"
+    );
+    searchInputElement.value = "dress";
+    fixture.detectChanges();
+
+    component.search.subscribe(
+      (enteredString: string) => (searchQuery = enteredString)
+    );
+
+    let searchIcon = fixture.debugElement.query(By.css("mat-icon"));
+    searchIcon.triggerEventHandler("click", null);
+    expect(searchQuery).toBe(expectedSearchQuery);
   });
 });
